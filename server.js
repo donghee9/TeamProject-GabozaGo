d5fc2e02f1d07c4ac2a1f3149077f421884928af
이주현 <leejoohyun@ijuhyeon-ui-MacBookPro.local>
2023-07-14 18:54:32 +0900

    초기 셋팅

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const { createApp } = require("./app");
+const DataSource = require("./api/models/dataSource");
+
+const startServer = async () => {
+  const app = createApp();
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, async () => {
+    console.log(`server listening on port ${PORT}`);
+    await DataSource.initialize()
+      .then(() => {
+        console.log("Data Source has been initialized!");
+      })
+      .catch((err) => {
+        console.error("Error during Data Source initialization:", err);
+      });
+  });
+};
+
+startServer();
