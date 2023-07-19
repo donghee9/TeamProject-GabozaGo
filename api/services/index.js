23e2afceb3e7d01f254cd0137b48490a9aadf3b7
donghee9 <seodonghee456@gmail.com>
2023-07-19 11:36:17 +0900

    ADD: 필터링 FIX:dao, where 절 추가 FIX:AUTH _추가

--- api/services/index.js
+++ api/services/index.js
@@ -1,11 +1,13 @@
 const spotsService = require("./spotsService");
 const userService = require("./userService");
 const activitiesService = require("./activitiesService");
-const reviewsService = require("./reviewsService.js")
+const reviewsService = require("./reviewsService.js");
+const storeActivityService = require("./storeActivityService");
 
 module.exports = {
   activitiesService,
   userService,
   spotsService,
-  reviewsService
+  reviewsService,
+  storeActivityService,
 };
