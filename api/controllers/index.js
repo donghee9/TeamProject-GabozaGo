4035075275e8de0200523d1a27fb9fb9b28f99e5
donghee9 <seodonghee456@gmail.com>
2023-07-20 10:32:06 +0900

    ADD: Get All Activities

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,7 +1,9 @@
 const userController = require("./userController");
 const spotsController = require("./spotsController");
+const activitiesController = require("./activitiesController");
 
 module.exports = {
+  activitiesController,
   spotsController,
   userController,
 };
